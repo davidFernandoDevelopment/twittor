6cc699c22e84c2f86a930b88cce9b30cfc41d264
David Sanchez <fernandoge210895@gmail.com>
2021-03-29 07:59:32 -0500

    test

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts("js/sw-utils.js")
 
 const STATIC_CACHE = "static-v2";
-const DYNAMIC_CACHE = "dynamic-v1";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
@@ -41,6 +41,10 @@ self.addEventListener("activate", e => {
         .then(keys => {
             keys.forEach(key => {
                 if (key.includes("static") && key !== STATIC_CACHE) {
+                    caches.delete(key);
+                }
+
+                if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                     caches.delete(key);
                 }
             })
